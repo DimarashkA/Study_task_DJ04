bdee83df3bb9da687e3485e2459e07a969edd169
DimarashkA <1969adv@list.ru>
2024-08-26 11:12:30 +0300

    add new project

--- movie_project/films/urls.py
+++ movie_project/films/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.film_list, name='film_list'),
     path('add/', views.add_film, name='add_film'),
+    path('', views.film_list, name='film_list'),
 ]
